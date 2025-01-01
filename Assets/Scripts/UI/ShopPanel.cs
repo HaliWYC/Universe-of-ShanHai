017d360c7ea56a6c2ca43ec85129262f0122529c
Hali <2545181786@qq.com>
2025-01-02 01:43:11 +0800

    Version2.31

--- Assets/Scripts/UI/ShopPanel.cs
+++ Assets/Scripts/UI/ShopPanel.cs
@@ -10,6 +10,8 @@ public class ShopPanel : MonoBehaviour
 
     public RectTransform CardShopHolder;
 
+    private List<CardDataSO> waitingCardList = new();
+
     public MultipleRarity shopRarity;
 
     public List<CardDataSO> cardList = new();
@@ -50,14 +52,15 @@ public class ShopPanel : MonoBehaviour
 
     private void InitCards()
     {
+        waitingCardList.Clear();
+        GenerateWaitingList();
         int count = 0;
-        for (int i = 0; i < numOfCard; i++)
+        for (int i = 0; i < waitingCardList.Count; i++)
         {
-
             var Card = Instantiate(CardPrefab, CardShopHolder.transform).GetComponent<ShopCardUI>();
             Card.gameObject.transform.localScale = Vector3.zero;
             Card.Parent = CardShopHolder;
-            Card.SetCardData(GetRandomCard());
+            Card.SetCardData(waitingCardList[i]);
             Card.isMoving = true;
             Card.gameObject.transform.DOScale(Vector3.one, 0.3f).SetEase(Ease.OutBack).SetDelay(i * 0.3f).onComplete = () =>
             {
@@ -68,8 +71,23 @@ public class ShopPanel : MonoBehaviour
                     backButton.interactable = true;
                 }
             };
+        }
+    }
 
-
+    private void GenerateWaitingList()
+    {
+        for (int i = 0; i < numOfCard; i++)
+        {
+            CardDataSO wait = GetRandomCard();
+            if (!CheckNoRepeatCard(wait))
+            {
+                waitingCardList.Add(wait);
+            }
+            else
+            {
+                wait = GetRandomCard();
+                waitingCardList.Add(wait);
+            }
         }
     }
 
@@ -77,5 +95,17 @@ public class ShopPanel : MonoBehaviour
     {
         int randomIndex = UnityEngine.Random.Range(0, cardList.Count);
         return cardList[randomIndex];
+    }
+
+    private bool CheckNoRepeatCard(CardDataSO cardDataSO)
+    {
+        for (int i = 0; i < waitingCardList.Count; i++)
+        {
+            if (waitingCardList[i] == cardDataSO)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
